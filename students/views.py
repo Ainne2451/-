8f09560c5ccfe7cd86ed70b60b1e238dd8081766
Ainne2451 <62133524+Ainne2451@users.noreply.github.com>
2020-05-22 10:28:12 +0800

    Add files via upload

---
+++ students/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from studentsapp.models import student
+from django.http import HttpResponse
+
+import logging
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse, HttpResponseBadRequest
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from datetime import datetime
+from flask import Flask
+from flask import request
+from flask import abort
+from linebot.models import *
+logger = logging.getLogger("django")
+
+"""
+line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
+parser  = WebhookParser(settings.LINE_CHANNEL_SECRET)
+"""
+app = Flask(__name__)
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('CxVO4fNcPJFe3hszmu//G4BnLFRyhwv2pByFcB6h48HeelcuweOrvwg2+VHahoocWQyKqMyFI+Px27xdcUTLSxI8qPNz9v2+16nC26iUgqvbjcj6Mbwe+tJ5sgcv4EvLNNg8vL7VYIF2zIUoA0/YHgdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('339f6e2054c10bc8f4fd19e2277fd4c8')
+
+line_bot_api.push_message('Uae6995ae4c73190deaa1d36b1c3f1a7b', TextSendMessage(text='你可以開始了'))
+
+@csrf_exempt
+@require_POST
+def callback(request):
+    signature = request.META['HTTP_X_Line_Signature']
+    body = request.body.decode('utf-8')
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        messages = ( "Invalid signature. Please check your channel access token/channel secret.")
+        HttpResponseForbidden()
+
+    return HttpResponse('OK',status=200)
+
+
+@handler.add(event=MessageEvent, message=TextMessage)
+def handl_message(event: MessageEvent):
+           line_bot_api.reply_message(
+            reply_token=event.reply_token,
+            messages=TextSendMessage(text=event.message.text),
+        )
